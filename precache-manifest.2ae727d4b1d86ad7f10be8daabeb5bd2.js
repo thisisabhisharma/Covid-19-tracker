f1de1ee9b2847f9cfad45f356f68aa06fff0fe96
thisisabhisharma <38296451+thisisabhisharma@users.noreply.github.com>
2020-08-17 12:34:21 +0530

    Updates

---
+++ precache-manifest.2ae727d4b1d86ad7f10be8daabeb5bd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8474ecd6bf948bcafdfa60b8a97d8ac2",
+    "url": "/Covid-19-tracker/index.html"
+  },
+  {
+    "revision": "c5aa564118d0b800debe",
+    "url": "/Covid-19-tracker/static/css/main.3c631858.chunk.css"
+  },
+  {
+    "revision": "53bd0baba80d38528e68",
+    "url": "/Covid-19-tracker/static/js/2.0f253384.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-tracker/static/js/2.0f253384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5aa564118d0b800debe",
+    "url": "/Covid-19-tracker/static/js/main.3cfe19ec.chunk.js"
+  },
+  {
+    "revision": "ccb9d7d70777db81e403",
+    "url": "/Covid-19-tracker/static/js/runtime-main.eba17668.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
